e93ad79a6fed8bc482306068777fb97c4d3d5a8b
kenny.nino <knino@t-idsolutions.com>
2024-02-03 17:02:52 -0500

    Cambios

---
+++ SamplePlugin.NFCBefore/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SamplePlugin.NFCBefore
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
